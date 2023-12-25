1fc6438e59c7a154e629be61a9a96d4455b54428
Sh3ikh-Sahab <sp22bsse0068@maju.edu.pk>
2023-12-25 23:44:12 +0500

    adding a project with my files

---
+++ music_and_movie_prj/src/music_and_movie_prj/user_info.java
@@ -0,0 +1,33 @@
+package music_and_movie_prj;
+import java.sql.*;
+
+public class user_info {
+	
+	public void getuserinfo(String Name,String password,String Email) {
+		try{
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con =
+			DriverManager.getConnection("jdbc:mysql://localhost:3306/music_movie","root","");
+			Statement stmt=con.createStatement();
+			
+			
+			stmt.executeUpdate("INSERT INTO `users` VALUES (NULL,'"+Name+"','"+password+"','"+Email+"')");
+			
+		
+			
+		
+			con.close();
+			//return user_info;
+		}
+		
+		catch(Exception e)
+		{
+		System.out.println(e);
+		}
+	
+	}
+	
+	
+	
+}
+
